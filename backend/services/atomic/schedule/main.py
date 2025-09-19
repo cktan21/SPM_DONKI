85f963c6b919f955f85b35b14df4cd3c3ac768a8
Kevin Tan <cekevintan@gmail.com>
2025-09-19 23:55:02 +0800

    feat: Added CRUD to schedule ms
    added small fixes,
    will finish up the testcase CI/CD integration tomrrow + compositie ms

--- backend/services/atomic/schedule/main.py
+++ backend/services/atomic/schedule/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Request
+from typing import Any, Dict
+from fastapi import FastAPI, HTTPException, Body
 from fastapi.responses import Response
 from supabaseClient import SupabaseClient
 from dotenv import load_dotenv
@@ -12,6 +13,50 @@ supabase = SupabaseClient()
 @app.get("/")
 def read_root():
     return {"message": "Schedule Service is running 🚀😌"}
+
+@app.get("/favicon.ico")
+async def get_favicon():
+    return Response(status_code=204)
+
+# Retrieve TID
+@app.get("/{tid}")
+def get_schedule(tid: str):
+    data = supabase.fetch_schedule(tid)
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Task {tid} not found")
+    return data
+
+# Create new Row
+@app.post("/")
+def insert_new_schedule(new_data: Dict[str, Any] = Body(...) ):
+    tid = new_data.get("tid")
+    deadline = new_data.get("deadline")
+    status = "ongoing"
+    try:
+        response = supabase.insert_schedule(tid, deadline, status)
+        return response.data
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+# Update the row
+@app.put("/{tid}")
+def update_schedule(tid: str, new_data: Dict[str, Any] = Body(...)):
+    try:
+        data = supabase.update_schedule(tid, new_data)
+        if not data:
+            raise HTTPException(status_code=404, detail=f"Task {tid} not found")
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+# Delete Row
+@app.delete("/{tid}")
+def delete_schedule(tid: str):
+    data = supabase.delete_schedule(tid)
+    if not data:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": f"Task {tid} deleted successfully"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5300)
--- backend/services/atomic/schedule/supabaseClient.py
+++ backend/services/atomic/schedule/supabaseClient.py
@@ -8,15 +8,30 @@ class SupabaseClient:
         self.key = os.getenv("SUPABASE_API_KEY")
         self.client: Client = create_client(self.url, self.key)
     
-    # Sample Code PLS CHANGE
-    def insert_recommendation(self, rec):
-        response = self.client.table("Recommendations").insert({
-            "uuid": rec.id,
-            "recommendation": rec.recommendations
+    # Insert Schedule
+    def insert_schedule(self, tid, deadline, status):
+        if self.fetch_schedule(tid) is not None: #Assumption is that one task cannot have MULTIPLE DEADLINES
+            Exception("Task with this ID already exists.")
+        response = self.client.table("SCHEDULE").insert({
+            "tid": tid,
+            "deadline": deadline,
+            "status": status
         }).execute()
         return response
 
-    def fetch_recommendation(self, id):
-        response = self.client.table("Recommendations").select("*").eq("uuid", id).execute()
+    # Get Schedule
+    def fetch_schedule(self, tid):
+        response = self.client.table("SCHEDULE").select("*").eq("tid", tid).execute()
         data = response.data
         return data[0] if data else None
+    
+    # Delete Schedule
+    def delete_schedule(self, tid):
+        response = self.client.table("SCHEDULE").delete().eq("tid", tid).execute()
+        data = response.data
+        return data[0] if data else None 
+    
+    # Update Schedule
+    def update_schedule(self, tid, updated_data):
+        response = self.client.table("SCHEDULE").update(updated_data).eq("tid", tid).execute()
+        return response.data
