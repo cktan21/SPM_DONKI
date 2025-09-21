df236dd58edf3a1e6ad3bfab0085f0e4d214651b
Rctoh <109146462+Rctoh@users.noreply.github.com>
2025-09-21 15:10:40 +0800

    Updated frontend using Shadcn for vue
    the original frontend folder
    temporarily renamed to frontend_old (can delete soon if no one objects the shadcn ui library)

---
+++ frontend/app/lib/utils.ts
@@ -0,0 +1,4 @@
+// frontendtest/lib/utils.ts
+export function cn(...classes: (string | boolean | undefined)[]) {
+    return classes.filter(Boolean).join(' ')
+}
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,10 +3,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/icon',
-    '@nuxt/eslint',
-    '@nuxt/test-utils',
-    '@nuxt/image'
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+
+    /**
+     * Directory that the component lives in.
+     * Keep it at root: components/ui
+     */
+    componentDir: './components/ui'
+  },
+
+  // Make sure Nuxt scans the root components folder
+  components: [
+    { path: '~/components', pathPrefix: false }
   ]
 })
