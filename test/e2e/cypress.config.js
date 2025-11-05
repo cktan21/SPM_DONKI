534944f9bb29d1f2c8e2c4239f4c78dc94809c0f
sw <shuwen.ng.2023@scis.smu.edu.sg>
2025-11-05 23:26:48 +0800

    e2e testing

---
+++ test/e2e/cypress.config.js
@@ -0,0 +1,9 @@
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    video: false,
+    chromeWebSecurity: false,
+    supportFile: "cypress/support/e2e.js",
+  },
+});
