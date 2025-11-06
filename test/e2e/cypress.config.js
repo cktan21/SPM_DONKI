de438f0f75cd0238cfeca4c73f6a13bbf3da1113
sw <shuwen.ng.2023@scis.smu.edu.sg>
2025-11-06 16:17:06 +0800

    add gen report e2e

--- test/e2e/cypress.config.js
+++ test/e2e/cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,5 +8,24 @@ module.exports = defineConfig({
     video: false,
     chromeWebSecurity: false,
     supportFile: "cypress/support/e2e.js",
+      
+    setupNodeEvents(on, config) {
+      // custom tasks for checking and clearing downloads
+      on("task", {
+        findDownloadedFile({ dir, ext }) {
+          const files = fs.readdirSync(dir);
+          const found = files.find((file) => file.endsWith(ext));
+          return found ? path.join(dir, found) : null;
+        },
+
+        clearDownloads(dir) {
+          fs.rmSync(dir, { recursive: true, force: true });
+          return null;
+        },
+      });
+
+      return config;
+    },
   },
 });
+
