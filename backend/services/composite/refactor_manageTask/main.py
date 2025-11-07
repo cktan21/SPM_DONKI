3cb6acfcaa783091b51f8d58201f7ff6adee358b
Kevin Tan <cekevintan@gmail.com>
2025-11-07 19:56:19 +0800

    added refactor to kong, kong used as facade

--- backend/services/composite/refactor_manageTask/main.py
+++ backend/services/composite/refactor_manageTask/main.py
@@ -79,7 +79,7 @@ async def health_check():
     return {
         "status": "healthy",
         "service": "manage-task composite service",
-        "version": "1.0.0"
+        "version": "2.0.0"
     }
 
 
